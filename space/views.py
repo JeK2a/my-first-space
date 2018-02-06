8475094f48d9f0b817ff206778b04a15eae483c9
JeK2a <vipjonpc@mail.ru>
2018-02-06 21:09:13 +0300

    Modified templates to display posts from database.

--- space/views.py
+++ space/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Space
 
 
 def post_list(request):
-    return render(request, 'space/message_list.html', {})
+    messages = Space.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'space/message_list.html', {'messages': messages})
