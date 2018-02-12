8f7454bfe94c3333bcccda9ff15ae843f9dfe5ea
JeK2a <vipjonpc@mail.ru>
2018-02-12 14:07:03 +0300

    Add Vue and all functions

--- space/urls.py
+++ space/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'^$', views.message_list, name='message_list'),
     url(r'^api/mark_read$', views.mark_read, name='message_id'),
     url(r'^False$', views.mark_all_not_read, name='message_id'),
+    url(r'^json$', views.json_out_all_messages, name='json_out_all_message'),
+    url(r'^api/get_messages$', views.json_out_new_messages, name='json_out_new_messages'),
 ]
--- space/views.py
+++ space/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
 from django.utils import timezone
 from .models import Space
+import json
 
 
 def message_list(request):
@@ -21,3 +23,22 @@ def mark_all_not_read(request):
         Space.objects.update_or_create(id=message.id, defaults={'read': False})
     messages = Space.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'space/message_list.html', {'messages': messages})
+
+
+def json_out_all_messages(request):
+    data = []
+    messages = Space.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    for message in messages:
+        if not message.read:
+            data.append({"id": message.id, "text": message.text})
+    return HttpResponse(json.dumps(data), content_type='application/json')
+
+
+def json_out_new_messages(request):
+    data = []
+    lastid = int(request.GET.get('last_id'))
+    messages = Space.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    for message in messages:
+        if (lastid < message.id) and (not message.read):
+            data.append({"id": message.id, "text": message.text})
+    return HttpResponse(json.dumps(data), content_type='application/json')
